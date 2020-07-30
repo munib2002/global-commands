b591e4294283f0d5de3c402bcb22925cc192a271
munib2002 <munib200002@gmail.com>
2020-07-30 12:10:16 +0500

    a

--- Rename files/script.js
+++ Rename files/script.js
@@ -3,11 +3,12 @@ const { argv } = require('yargs');
 
 module.exports = () => {
     const dirPath = argv.dirPath;
-    if (!dirPath || dirPath.includes('C')) return console.log('Plz provide a directory path, which is not in \'C\' drive, (--dirPath)');
+    if (!dirPath || dirPath.includes('C:')) return console.log('Plz provide a directory path, which is not in \'C\' drive, (--dirPath)');
     if (!argv.fileName && !argv.A) return console.log('Plz provide a file name, or provide \'A\' flag to use same name is the parent folder, (--fileName, -A)');
+    argv.start = argv.start || 1;
     argv.fileName = argv.fileName || `${dirPath.split('\\').pop()} -- ep`;
     const files = fs.readdirSync(dirPath) || [];
     files.forEach((c, i) => {
-        fs.renameSync(`${dirPath}\\${c}`, `${dirPath}\\${argv.fileName} ${i + 1}.${argv.ext || c.split('.').slice(1).join('.')}`);
+        fs.renameSync(`${dirPath}\\${c}`, `${dirPath}\\${argv.fileName} ${ argv.start + i}.${argv.ext || c.split('.').slice(1).join('.')}`);
     });
 }
