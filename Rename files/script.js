57b2f811db5532c6ba580bb1ce30bfa9b46eba56
Munib ur Rehman <munib200002@gmail.com>
2020-08-09 09:30:21 +0500

    a

--- Rename files/script.js
+++ Rename files/script.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 const { argv } = require('yargs');
 const fse = require('fs-extra');
+const path = require('path');
 
 module.exports = () => {
     const dirPath = argv.dirPath;
     if (!dirPath || dirPath.includes('C:')) return console.log('Plz provide a directory path, which is not in \'C\' drive, (--dirPath)');
-    if (!argv.fileName && !argv.A) return console.log('Plz provide a file name, or provide \'A\' flag to use same name is the parent folder, (--fileName, -A)');
+    if (!argv.fileName && !argv.A) return console.log('Plz provide a file name, or provide \'A\' flag to use same name as the parent folder, (--fileName, -A)');
     argv.start = argv.start || 1;
     argv.fileName = argv.fileName || `${dirPath.split('\\').pop()} -- ep`;
 
-    fse.copySync(dirPath, `${dirPath}\\old`);
+    fse.copySync(dirPath, path.join(dirPath, `..\\${dirPath.split('\\').pop()} old`));
     const files = fs.readdirSync(dirPath) || [];
-    
+    console.log(files.sort((a,b) => +b.match(/\d*/)[0] - (+a.match(/\d*/)[0])));
     files.forEach((c, i) => {
-        fs.renameSync(`${dirPath}\\${c}`, `${dirPath}\\${argv.fileName} ${ argv.start + i}.${argv.ext || c.split('.').slice(1).join('.')}`);
+        const newName = `${dirPath}\\${argv.fileName} ${argv.start + i}.${argv.ext || c.split('.').slice(1).join('.')}`;
+        if (fs.existsSync(newName)) return;
+        fs.renameSync(`${dirPath}\\${c}`, newName);
     });
 }
