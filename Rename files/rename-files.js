9ad4f3643f6b1b16192f5240fc2982810918b893
munib2002 <munib200002@gmail.com>
2020-08-14 08:37:29 +0500

    a

--- Rename files/rename-files.js
+++ Rename files/rename-files.js
@@ -7,35 +7,40 @@ const path = require('path');
 const readline = require('readline');
 const chalk = require('chalk')
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
 const dirPath = argv.dirPath;
 if (!dirPath || dirPath.includes('C:')) return console.log('Plz provide a directory path, which is not in \'C\' drive, (--dirPath)');
 if (!argv.fileName && !argv.A) return console.log('Plz provide a file name, or provide \'A\' flag to use same name as the parent folder, (--fileName, -A)');
 argv.start = argv.start || 1;
 argv.fileName = argv.fileName || `${dirPath.split('\\').pop()} -- ep`;
 
-fse.copySync(dirPath, path.join(dirPath, `..\\${dirPath.split('\\').pop()} old`));
+// fse.copySync(dirPath, path.join(dirPath, `..\\${dirPath.split('\\').pop()} old`));
 let files = fs.readdirSync(dirPath) || []; 
 
-const customSort = (arr = files) => [...arr].sort((a, b) => +a.match(/\d+(?=\..+)/)[0] - +b.match(/\d+(?=\..+)/)[0]);
+const customSort = (arr = files) => [...arr].sort((a, b) => +a.match(/\d+(?=\D*\..+)/)?.join() - +b.match(/\d+(?=\D*\..+)/)?.join());
 
 console.log(customSort());
 
 const rename = (doCustomSort = false) => {
     let newNames = [];
     if (doCustomSort) files = customSort();
+    const oldNamesPath = `C:\\Users\\Alpha\\Desktop\\Rename files Old\\${dirPath.split('\\').pop()}.txt`;
+    if (!fs.existsSync(oldNamesPath)) fs.writeFileSync(oldNamesPath, '');
+    fs.appendFileSync(oldNamesPath, JSON.stringify({names: files, time: new Date()}));
+
     files.forEach((c, i) => {
         const newName = `${dirPath}\\${argv.fileName} ${argv.start + i}.${argv.ext || c.split('.').slice(1).join('.')}`;
         newNames.push(newName)
-        if (fs.existsSync(newName)) return;
+        if (fs.existsSync(newName) || argv.newNames) return;
 		fs.renameSync(`${dirPath}\\${c}`, newName);
     });
-    console.log(chalk.bold.hex('#0c2461').bgHex('#25CCF7')(`            \r\n  Renamed!  \r\n            `));
+
+    console.log(argv.newNames? newNames : chalk.bold.hex('#0c2461').bgHex('#25CCF7')(`            \r\n  Renamed!  \r\n            `));
 }
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
 
 rl.question(chalk.bold.hex('#F8EFBA')("Input 'y' to rename in the above order: "), ans => {
     if (ans != 'y') {
@@ -49,3 +54,4 @@ rl.question(chalk.bold.hex('#F8EFBA')("Input 'y' to rename in the above order: "
         rl.close();
     }
 });
+
